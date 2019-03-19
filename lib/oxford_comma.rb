8dabe5e6d33a1044a7a1eb399f3c54424eef0b10
SOYEONG OH <axionoso@gmail.com>
2019-03-19 20:06:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length <= 2
+    array.join(" and ")
+  elsif array.length == 3
+    array.join(", ").split(" ").insert(-2, "and").join(" ")
+  elsif array.length > 3
+    str = ", and " + array.pop
+    array.join(", ") << str
+  end
 end
