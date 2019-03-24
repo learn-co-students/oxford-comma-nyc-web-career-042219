d51c08dfcd75cbcee3fa4b86d90b9b7ae7557917
Jason Gomez <gmzjsn@gmail.com>
2019-03-24 02:13:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  return array[0] if array.length == 1 
+  return "#{array[0]} and #{array[1]}" if array.length == 2
+  # string = array.join(", ")
+  # string = string.split
+  # string[string.length - 1] = "and " + string[string.length - 1]
+  # string = string.join(" ")
+  array[array.length - 1] = "and " + array[array.length - 1]
+  array.join(", ")
 end
