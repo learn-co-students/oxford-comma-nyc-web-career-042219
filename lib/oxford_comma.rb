9b284d04f09456361a873fd43737e950e846eeb3
Adam Sultanov <adam.sultanov@gmail.com>
2019-04-01 13:26:05 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+size = array.size
+last = ""
+
+if size > 2
+  last = ", and "
+  last << array.slice!(-1)
+elsif size == 2
+  last = " and "
+  last << array.slice!(-1)
+end
+
+  array.join(", ") + last
 end
