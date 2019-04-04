ea7cc2a2e8b3e9e49c042797e3464745a6ebca6d
Won Kim <wonsae.kim@gmail.com>
2019-04-04 05:03:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+  msg = ""
+  more_than_2 = false
+  while !array.empty?
+    msg += array.shift
+    
+    if array.length != 0
+      if array.length > 1
+        msg += ", "
+        more_than_2 = true
+      else
+        if more_than_2 == true
+          msg += ", and "
+        else
+          msg += " and "
+        end
+      end
+    end
+  end
+  msg
 end
