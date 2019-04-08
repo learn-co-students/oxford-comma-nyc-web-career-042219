627f8f7087f2534257b16e78281c574c05f061ee
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-08 20:50:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,12 @@
-def oxford_comma(array)
-array.join(",")
-array.join(" and ")
 
+def oxford_comma(array)
+if array.length == 1 
+  array.join
+elsif array.length == 2 
+  array[-2] <<  " and "
+  array.join
+elsif array.length > 2 
+  array[-1].prepend "and "
+  array.join(", ")
+end
 end
