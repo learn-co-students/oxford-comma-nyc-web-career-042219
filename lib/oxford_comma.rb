79bf583cd1d861356313a0ca501bfd3dab160451
Christian Pinto <pintochristian2@gmail.com>
2019-04-21 01:10:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  size = array.size
+  if size  == 1
+    return array.join("")
+  elsif size == 2
+    return array.join(' and ')
+    elsif size == 3
+      string = ""
+      string << array.join(", ")
+      string.insert(14, "and ")
+      string
+    elsif size > 3
+    array.insert(size - 1, "and")
+     list =  array.join(", ")
+     comma = list.index("and")
+     list[comma + 3] = ''
+     list
+    end
 end
